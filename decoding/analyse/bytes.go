be2c809e468d8bb4f647e8c74e33c988e399da8d
Robin Harrison <merlinc@courts.io>
2021-05-13 18:48:51 +0100

    feat: Add decoding from clacks-camera
    Repackage clacks-camera/analyse into clex/decoding

---
+++ decoding/analyse/bytes.go
@@ -0,0 +1,63 @@
+// Package analyse extracts textual clacks messages from gocv.Mat
+package analyse
+
+import (
+	"gocv.io/x/gocv"
+	"image/color"
+	"math/big"
+)
+
+// Color of the marked points
+var pointColor = color.RGBA{0, 255, 255, 0}
+
+func GetBytes(mat *gocv.Mat) string {
+	if mat.Empty() {
+		return ""
+	}
+	thresh := gocv.NewMat()
+	defer thresh.Close()
+
+	var weakThreshold float32 = 127
+	var strongThreshold float32 = 255
+	gocv.Threshold(*mat, &thresh, weakThreshold, strongThreshold, gocv.ThresholdBinary)
+
+	thresh.CopyTo(mat)
+
+	size := mat.Size()
+	pixelWidth := size[1] / 2
+	pixelHeight := size[0] / 4
+	pixelXOffset := pixelWidth / 2
+	pixelYOffset := pixelHeight / 2
+
+	ch := mat.Channels()
+	v := make([]uint8, ch)
+
+	y := pixelYOffset
+	x := pixelXOffset
+
+	bi := big.NewInt(0)
+
+	var b uint = 0
+
+	for row := 0; row < 4; row++ {
+		for col := 0; col <2; col++ {
+			y = row*pixelHeight+pixelYOffset
+			x = col*pixelWidth+pixelXOffset
+
+			for c := 0; c < ch; c++ {
+				//fmt.Printf("%v", c)
+				v[c] = mat.GetUCharAt(y, x*ch+c)
+			}
+
+			if(v[0] == 255) {
+				b = 1
+			} else {
+				b = 0
+			}
+
+			bi.SetBit(bi, row*2+col, b)
+		}
+	}
+
+	return string(bi.Bytes())
+}
---
+++ decoding/overlay/contour.go
@@ -0,0 +1,41 @@
+package overlay
+
+import (
+	//"fmt"
+	"gocv.io/x/gocv"
+)
+
+const MinimumArea = 1
+
+
+// Applies a countour line around supplied contours
+func ApplyTracing(img *gocv.Mat, contours gocv.PointsVector) {
+	//fmt.prin
+
+	for i := 0; i < contours.Size(); i++ {
+		c := contours.At(i)
+		//fmt.Println(i)
+		//fmt.Println(c)
+		area := gocv.ContourArea(c)
+		//area := gocv.ContourArea(c)
+		if area < MinimumArea {
+			continue
+		}
+
+		//status: = "Edges detected"
+		gocv.DrawContours(img, contours, i, contourColor, 2)
+		rect := gocv.BoundingRect(c)
+		gocv.Rectangle(img, rect, bboxColor, 2)
+
+		rotrect := gocv.MinAreaRect(c)
+		vertices := rotrect.Points
+		for i = 0; i < 4; i++ {
+			gocv.Line(img, vertices[i], vertices[(i+1)%4], rotatedbboxColor, 2)
+		}
+
+		//gocv.BoundingRect(contours)
+		//gocv.Rectangle(img, rect, bboxColor, 2)
+
+	}
+}
+
